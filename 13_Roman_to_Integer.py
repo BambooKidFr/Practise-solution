b306777b5a6a07b42d9df4e2e4f4e7ec32ce03ef
BambooKidFr <manhqh0909@gmail.com>
2020-05-10 10:24:07 +0700

    Add Solution

---
+++ 13_Roman_to_Integer.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/roman-to-integer/
+
+class Solution:
+    def romantoInt(self, s: str) -> int:
+        doubles = {'CM': 900, 'CD': 400, 'XC': 90, 'XL': 50, 'IX': 9, 'IV': 4}
+        singles = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+
+        integer = 0
+        i = 0
+        while i < len(s):
+            if i < len(s) - 1 and s[i:i + 2] in doubles:
+                integer += doubles[s[i:i + 2]]
+                i += 2
+            else:
+                integer += singles[s[i]]
+                i += 1
+        return integer
+
+
+f = Solution()
+a = f.romantoInt(19)
+print(a)
